d54c7475a3c7e503aa677f38d8b15f9e7f6ab699
DusicaZeravic <dukizeravic@gmail.com>
2020-12-10 14:17:44 +0100

    changed fourth task

--- 2. nedelja/Sreda/zadatak4.js
+++ 2. nedelja/Sreda/zadatak4.js
@@ -1,4 +1,11 @@
 // Исписати првих 1000 бројева дељивих са 5
-for(let i = 1; i <= 1000; i++) {
-    if(i % 5 == 0) console.log(i);
+let num = 0;
+for(let i = 1;; i++) {
+     if(i % 5 == 0) {
+         console.log(i);
+         num ++;
+     }
+     if(num > 1000) {
+         break;
+     }
 }
